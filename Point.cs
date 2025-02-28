52169fc5aa6988a0dc9186681016f5caf40992c2
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-02-28 13:48:19 +0300

    num2

---
+++ Point.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace prac26._2
+{
+    internal class Point
+    {
+        private double x,y;
+        public static double count;
+
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+
+        public Point(double x, double y) 
+        {
+            this.X = x;
+            this.Y = y;
+            count++;
+        }
+        public Point()
+        {
+            this.X = 1.0;
+            this.Y = 1.0;
+            count++;
+        }
+        static Point()
+        {
+            count = 0;
+        }
+
+        public double GetX()
+        {
+            return x;
+        }
+        public double GetY()
+        {
+            return y;
+        }
+        public void SetX(double x)
+        {
+            this.x=x;
+        }
+        public void SetY(double y)
+        {
+            this.y=y;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"x={GetX()},y={GetY()}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace prac26._2
     {
         static void Main(string[] args)
         {
+            Point point1 = new Point(2.1,3.2);
+            point1.Print();
+            point1.SetX(1.2);
+            point1.SetY(2.3);
+            Console.WriteLine("Обновленно значение X,Y");
+            point1.Print();
+
         }
     }
 }
